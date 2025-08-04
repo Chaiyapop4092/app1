e83723ea4ac7049f2af2573abd6749ef064f41f2
Chaiyapop4092 <s6503051624092@email.kmutnb.ac.th>
2025-08-04 09:49:40 +0700

    ปัญหาการใช้ this ใน Class Component

--- src/class-components.js
+++ src/class-components.js
@@ -1,23 +1,16 @@
-import React, {Component} from 'react'
-export default class Calendar extends Component {
-    getDate() {
-        const dayNames = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 
-                          'เสาร์']
+import React, { Component } from 'react';
 
-        const monthNames = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน',
-                            'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 
-                            'กันยนยน', 'ตุลาคม','พฤศจิกายน', 'ธันวาคม']
+export default class Button extends Component {
+    showAlert(msg) {
+        alert(msg);
+    }
 
-        const date = new Date()
-        const weekDay = dayNames[date.getDay()]
-        const day = date.getDate()
-        const month = monthNames[date.getMonth()]
-        const year = date.getFullYear() + 543
+    onClickButtonOK = () => {
+        // Event Handler แบบ Arrow Function
+        this.showAlert('Hello'); // ใช้ this ได้เลย โดยไม่เกิดปัญหา
+    }
 
-        return `วัน${weekDay} ที่ ${day} ${month} ${year}`
-    }// alt + 96 for backtick `
-        
     render() {
-        return <div>{this.getDate()}</div>
+        return <button onClick={this.onClickButtonOK}>OK</button>;
     }
 }
